0dba31095baa31ee4dcd3628fc571d4282bc78e1
Martin Novák <martin.novak@gmx.com>
2025-01-11 19:50:41 +0100

    Use average of raw and static eval for corrhist update
    Bench: 1183419

--- src/search.cpp
+++ src/search.cpp
@@ -1440,7 +1440,8 @@ moves_loop:  // When in check, search starts here
         const auto       m             = (ss - 1)->currentMove;
         static const int nonPawnWeight = 154;
 
-        auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / 8,
+        const auto evalAvg = int((ss->staticEval + unadjustedStaticEval) / 2);
+        auto bonus = std::clamp(int(bestValue - evalAvg) * depth / 8,
                                 -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
         thisThread->pawnCorrectionHistory[us][pawn_structure_index<Correction>(pos)]
           << bonus * 107 / 128;
