35abe28c392d10e54e0058160a4463bd1e41cf15
Martin Novák <martin.novak@gmx.com>
2025-01-12 10:26:40 +0100

    Different weight of contcorr -3 -1
    Bench: 1382045

--- src/search.cpp
+++ src/search.cpp
@@ -91,10 +91,10 @@ int correction_value(const Worker& w, const Position& pos, Stack* ss) {
                  : 0;
 
     const auto  cntcv2 =
-      m2.is_ok() ? (*(ss - 3)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
+      (m.is_ok() && m2.is_ok()) ? (*(ss - 3)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
                  : 0;
 
-    return (6300 * pcv + 3500 * macv + 6400 * micv + 6700 * (wnpcv + bnpcv) + 5400 * (cntcv + cntcv2));
+    return (6384 * pcv + 3583 * macv + 6492 * micv + 6725 * (wnpcv + bnpcv) + 5880 * cntcv + 3583 * cntcv2);
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation
@@ -1460,7 +1460,7 @@ moves_loop:  // When in check, search starts here
         if (m.is_ok())
             (*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()] << bonus;
 
-        if (m2.is_ok())
+        if (m.is_ok() && m2.is_ok())
             (*(ss - 3)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()] << bonus;
     }
 
